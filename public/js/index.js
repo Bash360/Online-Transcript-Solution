846cbce7da6a57975c24a4dd009550d6e9afe32d
boscoseries <boscoseries@gmail.com>
2019-02-26 13:05:04 +0100

    implemented post requests to /signup

--- public/js/index.js
+++ public/js/index.js
@@ -1,19 +1,36 @@
-$(document).ready(function() {
-  //On login page
-  $("#signup_form").click(function() {
-    $("#login_container").fadeIn();
-    //$("#signup_container").fadeIn();
-  })
+$(document).ready(function () {
 
-  //On signup page
-  $("#login_form").click(function() {
-    $("#signup_container").hide();
-    $("#login_container").show();
-  })
+  $("#target").submit(function (event) {
+
+    var fullname = $("#fullname").val();
+    var id = $("#id").val();
+    var password = $("#password").val();
+    var compare_password = $("#compare_password").val();
+
+    if (password === compare_password) {
+      
+
+    $.ajax({
+      method: "POST",
+      url: "http://localhost:3000/users",
+      data: {
+        fullname: fullname,
+        id: id,
+        password: password,
+        compare_password: compare_password
+      },
+      success() {
+        window.location.assign("dashboard.html");
+      }
+    });
+    event.preventDefault();
+  }
+
+  });
+  
 
 
+  
 
 
-
-
-})
+});
