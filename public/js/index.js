9ce5ffe9b5ec8b25be6be09e6f1da8b4700a5384
boscoseries <boscoseries@gmail.com>
2019-02-28 14:49:43 +0100

    added UI pages for admin users

--- public/js/index.js
+++ public/js/index.js
@@ -1,56 +1,11 @@
 
 $(document).ready(function () {
 
-  $("#login_submit").submit(function (event) {
-
-    var fullname = $("#fullname").val();
-    var id = $("#id").val();
-    var password = $("#password").val();
-    var compare_password = $("#compare_password").val();
-
-    if (password === compare_password) {
-
-      $.ajax({
-        method: "POST",
-        url: "http://localhost:3000/users",
-        data: {
-          fullname: fullname,
-          id: id,
-          password: password,
-          compare_password: compare_password
-        },
-        success: function() {
-          window.location.assign("dashboard.html");
-        }
-      });
-      event.preventDefault();
+  let url = new URLSearchParams(window.location.search)
+    url.has("username")
+    let params = url.get("username") 
+    if (params == null) {
+      window.location.assign("index.html")
     }
 
   })
-
-
-
-  $("#login_login").submit(function (event) {
-
-    event.preventDefault();
-
-    var id = $("#id").val();
-    var password = $("#password").val();
-
-      $.ajax({
-        method: "GET",
-        url: "http://localhost:3000/users",
-        dataType: "json",
-        data: {id: id, password: password },
-        success(res) {
-          if (!res.length) {
-              return false
-          }
-          window.location.assign(`dashboard.html?id=${id}`);
-        }
-      });
-     
-    })
-
-
-});
---
+++ public/js/script.js
@@ -0,0 +1,61 @@
+
+$(document).ready(function () {
+
+  $("#submit_signup").submit(function (event) {
+
+    event.preventDefault();
+
+    var fullname = $("#fullname").val();
+    var email = $("#email").val();
+    var student_id = $("#student_id").val();
+    var department = $("#dept").val();
+    var year_of_entry = $("#yoe").val();
+    var year_of_graduation = $("#yog").val();
+    var password = $("#password").val();
+
+      $.ajax({
+        method: "POST",
+        url: "http://localhost:3000/users",
+        data: {
+          fullname: fullname,
+          email: email,
+          student_id: student_id,
+          department: department,
+          yearOfEntry: year_of_entry,
+          yearOfGraduation: year_of_graduation,
+          password: password
+        },
+        success: function() {
+          window.location.assign("dashboard.html");
+        }
+      });
+    //}
+
+  })
+
+
+
+  $("#login_login").submit(function (event) {
+
+    event.preventDefault();
+
+        student_id = $("#student_id").val();
+    var password = $("#password").val();
+
+      $.ajax({
+        method: "GET",
+        url: "http://localhost:3000/users",
+        dataType: "json",
+        data: {student_id: student_id, password: password },
+        success(res) {
+                 if (!res.length) {
+              return false
+          }
+          window.location.assign(`dashboard.html?id=${student_id}`);
+        }
+      });
+     
+    })
+
+
+});
