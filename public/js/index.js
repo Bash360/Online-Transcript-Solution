c3337988b673b29c3de24562fa29ece9b5506bef
boscoseries <boscoseries@gmail.com>
2019-02-28 08:47:54 +0100

    added request transcript page

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,7 @@
+
 $(document).ready(function () {
 
-  $("#target").submit(function (event) {
+  $("#login_submit").submit(function (event) {
 
     var fullname = $("#fullname").val();
     var id = $("#id").val();
@@ -8,29 +9,48 @@ $(document).ready(function () {
     var compare_password = $("#compare_password").val();
 
     if (password === compare_password) {
-      
 
-    $.ajax({
-      method: "POST",
-      url: "http://localhost:3000/users",
-      data: {
-        fullname: fullname,
-        id: id,
-        password: password,
-        compare_password: compare_password
-      },
-      success() {
-        window.location.assign("dashboard.html");
-      }
-    });
+      $.ajax({
+        method: "POST",
+        url: "http://localhost:3000/users",
+        data: {
+          fullname: fullname,
+          id: id,
+          password: password,
+          compare_password: compare_password
+        },
+        success: function() {
+          window.location.assign("dashboard.html");
+        }
+      });
+      event.preventDefault();
+    }
+
+  })
+
+
+
+  $("#login_login").submit(function (event) {
+
     event.preventDefault();
-  }
 
-  });
-  
+    var id = $("#id").val();
+    var password = $("#password").val();
 
-
-  
+      $.ajax({
+        method: "GET",
+        url: "http://localhost:3000/users",
+        dataType: "json",
+        data: {id: id, password: password },
+        success(res) {
+          if (!res.length) {
+              return false
+          }
+          window.location.assign(`dashboard.html?id=${id}`);
+        }
+      });
+     
+    })
 
 
 });
