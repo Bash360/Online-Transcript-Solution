40cdb6fe2ec03b9913c7d8dd826dc1e20c16cea9
mark bashir <l5richmond@L5s-MacBook-Pro-2.local>
2019-02-28 21:31:54 +0100

    changes made

--- public/js/generateData.js
+++ public/js/generateData.js
@@ -1,34 +1,34 @@
 function generateResult(department){
 if(department=="CSC"){
-  const courses = { course: [] }
+  const courses =[];
   counter=0;
   for (let i = 1; i <=48; i++) {
-    courses.course.push({ courseCode: `CSC${i}${counter}`, score: Math.floor((Math.random() * 70) +30),courseUnit:3 });
+    courses.push({ courseCode: `CSC${i}${counter}`, score: Math.floor((Math.random() * 70) +30),courseUnit:3 });
 	counter++;
   }
   return courses;
 }
 if(department=="ICT"){
-  const courses = { course: [] }
+  const courses =[];
   counter=0;
   // auto generate ICT courses and grades
   for (let i = 1; i <=48; i++) {
-    courses.course.push({ courseCode: `ICT${i}${counter}`, score: Math.floor((Math.random() * 70) + 30),courseUnit:3 });
+    courses.push({ courseCode: `ICT${i}${counter}`, score: Math.floor((Math.random() * 70) + 30),courseUnit:3 });
 	counter++;
   }
   return courses;
 }
 if(department=="ENG"){
-  const courses = { course: [] }
+  const courses =[];
   counter=0;
   // auto generate computer engineering courses and grade
   for (let i = 1; i <=60;i++) {
 	  
-    courses.course.push({ courseCode: `Eng${i}${counter}`, score: Math.floor((Math.random() * 70) + 30),courseUnit:3 });
+    courses.push({ courseCode: `Eng${i}${counter}`, score: Math.floor((Math.random() * 70) + 30),courseUnit:3 });
 	counter++;
   }
   return courses;
- //console.log(courses.course[50].score);
+
 }
 
 }
