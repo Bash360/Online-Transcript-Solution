c764f034cba7a8343e83ce6238c88292240ba837
boscoseries <boscoseries@gmail.com>
2019-03-11 12:09:46 +0100

    heroku bug fix

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const jsonServer = require('json-server')
 const server = jsonServer.create()
 const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 
 
 server.use(middlewares)
